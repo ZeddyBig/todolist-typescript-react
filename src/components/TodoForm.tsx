79e104cd86b4f71e313f374c9c26ecd8d85fed23
Yuri Chetverikov <bashered@gmail.com>
2022-10-31 22:00:15 +0300

    add: Сделать основную форму

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,30 @@
+import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
+
+interface TodoFormProps {
+    addTodo: AddTodo;
+}
+
+export const TodoForm: FC<TodoFormProps> = ({ addTodo }) => {
+    const [newTodo, setNewTodo] = useState<string>('');
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTodo(e.target.value);
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        addTodo(newTodo);
+        setNewTodo('');
+    }
+
+    return (
+        <form className='todo-form' onSubmit={handleSubmit}>
+            <input type='text' value={newTodo} className='todo-input' placeholder='Add a task' onChange={handleChange}/>
+            <button type='submit' className='todo-button'>
+                Add a task
+            </button>
+        </form>
+    );
+};
+
+export default TodoForm;
